84c10007a847cfc530ff90cfad768e1c42d5feab
Muhammad Muzammil <43114403+Muzz07@users.noreply.github.com>
2021-02-10 06:28:36 -0800

    Quotes API

--- app/Http/Controllers/api/QuotesController.php
+++ app/Http/Controllers/api/QuotesController.php
@@ -13,15 +13,19 @@ class QuotesController extends Controller
     	return Quotes::orderBy($sortcol,$sorttype)->paginate($perpage);
     }
     public function create_or_update(Request $request,Quotes $quotes){
-        
+   
     	$quotes->firstname=$request->firstname;
     	$quotes->lastname=$request->lastname;
     	$quotes->email=$request->email;
     	$quotes->website=$request->website;
     	$quotes->qoute_status=$request->qoute_status;
     	if($quotes->save()){
-            $quotes_item = new Quotes_item;
             foreach ($request->items as $item) {
+                if(isset($item['id'])&&intval($item['id'])>0){
+                    $quotes_item = Quotes_item::find($item['id']);
+                }else{
+                    $quotes_item = new Quotes_item;
+                }
                 $quotes_item->item=$item['item'];
                 $quotes_item->sku=$item['sku'];
                 $quotes_item->qty=$item['qty'];
