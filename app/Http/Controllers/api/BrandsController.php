84c898cd6c12a05ed25bf8679c1a57c28fd8f5e7
Muhammad Muzammil <43114403+Muzz07@users.noreply.github.com>
2021-02-10 05:21:05 -0800

    file storage and delete on edit, create and delete of brands

--- app/Http/Controllers/api/BrandsController.php
+++ app/Http/Controllers/api/BrandsController.php
@@ -13,10 +13,13 @@ class BrandsController extends Controller
         return Brands::orderBy($sortcol,$sorttype)->paginate($perpage);
     }
     public function create_or_update(Request $request,Brands $brands){
+        if(intval($brands->id)>0){
+            Storage::delete($brands->image);}
     	$brands->name=$request->name;
         $path = $request->file('image')->store('brands');
     	$brands->image=$path;
     	if($brands->save()){
+
     		return response()->json(['status'=>1]);
     	}
     	else{
@@ -24,6 +27,7 @@ class BrandsController extends Controller
     	}
     }
     public function delete(Brands $brands){
+        Storage::delete($brands->image);
     	$response = $brands->delete();
     	return response()->json(['status'=>1]);
     }
