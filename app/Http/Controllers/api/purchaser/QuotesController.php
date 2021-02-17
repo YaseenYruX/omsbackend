20f56f6c46f24b4d84b6b61c7a4c670459722d72
Muhammad Muzammil <muhammad.muzammil@bsdtechs.com>
2021-02-17 07:45:19 -0800

    Update QuotesController.php

--- app/Http/Controllers/api/purchaser/QuotesController.php
+++ app/Http/Controllers/api/purchaser/QuotesController.php
@@ -16,7 +16,8 @@ class QuotesController extends Controller
         $perpage=!empty($_GET['perpage'])?intval($_GET['perpage']):20;
         $sortcol=!empty($_GET['sortcol'])?$_GET['sortcol']:'id';
         $sorttype=!empty($_GET['sorttype'])?$_GET['sorttype']:'desc';
-    	return Quotes::orderBy($sortcol,$sorttype)->where('qoute_status','Open')->paginate($perpage);
+    	return Quotes::orderBy($sortcol,$sorttype)//->where('qoute_status','Open')
+        ->paginate($perpage);
     }
 
     public function itemquote(Request $request,Quotes_item $quotes_item)
